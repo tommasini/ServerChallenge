538185183277f572cc0ffd16d5b38b464f79afae
tomas <tommasini15@gmail.com>
2020-07-09 00:02:57 +0100

    First migration okay!
    First migration, table name = tasks, database is inside an image of docker

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      port: "5432",
+      host: "192.168.99.100",
+      user: "postgres",
+      password: "docker",
+      database: "lisgo"
+    },
+    migrations: {
+      directory: "./src/database",
+      tableName: "knex_migrations",
+    },
+  },
+};
--- src/server.js
+++ src/server.js
@@ -1,31 +1,25 @@
 'use strict';
+import Hapi from '@hapi/hapi';
+import routes from './routes';
 
-const Hapi = require('@hapi/hapi');
+const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+});
+
+server.route(routes);
 
 const init = async () => {
 
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
-    });
-    
-    server.route({
-      method: 'GET',
-      path: '/',
-      handler: (request, h) => {
-
-          return 'Hello World!';
+    try {
+        await server.start();
       }
-  });
-
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
+      catch (err) {
+        console.log(err);
+        process.exit(1);
+      }
+    
+      console.log('Server running at:', server.info.uri);
 };
-
-process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
-});
 
 init();
