90c545166da793204af4c801aa005846ca26ea2d
tomas <tommasini15@gmail.com>
2020-07-27 15:42:02 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -11,6 +11,13 @@ server.route(routes);
 
 const init = async () => {
   try {
+    await server.register({
+      plugin: require('hapi-cors'),
+      options: {
+          origins: ['http://localhost:3000']
+      }
+  });
+  
     await server.start();
   } catch (err) {
     console.log(err);
